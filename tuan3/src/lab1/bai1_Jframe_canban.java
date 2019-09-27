f86ddc763a3d90144a9d70a2a6609a548112758d
tranbaloc030799 <55137813+tranbaloc030799@users.noreply.github.com>
2019-09-27 21:31:56 +0700

    tuan3

---
+++ tuan3/src/lab1/bai1_Jframe_canban.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author ASUS
+ */
+public class bai1_Jframe_canban extends JFrame{
+
+    public bai1_Jframe_canban(){
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new  bai1_Jframe_canban();
+       
+    }
+}
